dd6afd7739b56a49e8faf83450a34a0a76a40b32
tianping <ggtianping@gmail.com>
2020-03-06 01:48:34 +0800

    ‘mcalculator

--- pages/calculator/calculator.js
+++ pages/calculator/calculator.js
@@ -1,11 +1,28 @@
-// const express = require('express');
-var data ={
-  name: '甜萍',
-  gender:[
-    {name:'男' , value: '0', checked:'false'},
-    {name:'女' , value: '1', checked:'true'}
-  ]
-}
 Page({ 
+  data:{
+    num:'123',
+    op:'+',
+   
+  },
+  result:null,
+  isClear:false,
+  numBtn: function(e){
+    var num = e.target.dataset.val;
+    if(this.data.num ==='0' || this.isClear){
+      this.setData({num:num})
+      this.isClear = false
+    }else{
+      this.setData({num:this.data + num})
+    }
+    console.log(num);
+  },
+  opBtn: function(e){
+    var op = e.target.dataset.val;
+    console.log(op);
+  },
+  dotBtn: function(e){
+    var dot = e.target.dataset.val;
+    console.log(dot);
+  }
 
-  })
+})
